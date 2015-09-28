64a197565e05df6053f639158bc44ea9733cad98
daokui0202 <daokui0202@163.com>
2015-09-28 10:47:58 +0800

    新建一个项目
    新建一个项目

---
+++ git的使用/git的使用/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  git的使用
+//
+//  Created by wawa on 15/9/28.
+//  Copyright © 2015年 wawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ git的使用/git的使用/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  git的使用
+//
+//  Created by wawa on 15/9/28.
+//  Copyright © 2015年 wawa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"hahahaha");
+}
+
+@end
